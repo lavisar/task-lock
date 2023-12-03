069b3f8c1cd45ac9cda5690778b47d05d7be3464
lavisar <sarlavi6969@gmail.com>
2023-12-03 21:34:46 +0700

    validate code

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -45,14 +45,14 @@ export const FromPicker = ({ id, errors }: FormPickerProps) => {
   }, [])
   if (isLoading) {
     return (
-      <div className="p-6 flex items-center justify-center">
-        <Loader2 className="h-6 w-6 text-sky-700 animate-spin" />
+      <div className='p-6 flex items-center justify-center'>
+        <Loader2 className='h-6 w-6 text-sky-700 animate-spin' />
       </div>
     )
   }
   return (
-    <div className="relative">
-      <div className="grid grid-cols-3 gap-2 mb-2">
+    <div className='relative'>
+      <div className='grid grid-cols-3 gap-2 mb-2'>
         {images.map(image => (
           <div
             key={image.id}
@@ -66,31 +66,36 @@ export const FromPicker = ({ id, errors }: FormPickerProps) => {
             }}
           >
             <input
-              type="radio"
+              type='radio'
               id={id}
               name={id}
-              className="hidden"
+              className='hidden'
               checked={selectedImageId === image.id}
               disabled={pending}
               value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
             />
-            <Image src={image.urls.thumb} alt="Unplash Image" className="object-cover rounded-sm" fill />
+            <Image
+              src={image.urls.thumb}
+              alt='Unplash Image'
+              className='object-cover rounded-sm'
+              fill
+            />
             {selectedImageId === image.id && (
-              <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
-                <Check className="h-4 w-4 text-white" />
+              <div className='absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center'>
+                <Check className='h-4 w-4 text-white' />
               </div>
             )}
             <Link
               href={image.links.html}
-              target="_blank"
-              className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
+              target='_blank'
+              className='opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50'
             >
               {image.user.name}
             </Link>
           </div>
         ))}
       </div>
-      <FormErrors id="image" errors={errors} />
+      <FormErrors id='image' errors={errors} />
     </div>
   )
 }
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -19,7 +19,12 @@ interface FormPopoverProps {
   sideOffset?: number
 }
 
-export const FormPopover = ({ children, side = 'bottom', align, sideOffset = 0 }: FormPopoverProps) => {
+export const FormPopover = ({
+  children,
+  side = 'bottom',
+  align,
+  sideOffset = 0
+}: FormPopoverProps) => {
   const router = useRouter()
   const closeRef = useRef<ElementRef<'button'>>(null)
 
@@ -42,19 +47,22 @@ export const FormPopover = ({ children, side = 'bottom', align, sideOffset = 0 }
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
-      <PopoverContent align={align} className="w-80 pt-3" side={side} sideOffset={sideOffset}>
-        <div className="text-sm font-medium text-center text-neutral-600 pb-4">Create board</div>
+      <PopoverContent align={align} className='w-80 pt-3' side={side} sideOffset={sideOffset}>
+        <div className='text-sm font-medium text-center text-neutral-600 pb-4'>Create board</div>
         <PopoverClose ref={closeRef} asChild>
-          <Button variant="ghost" className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600">
-            <X className="h-4 w-4" />
+          <Button
+            variant='ghost'
+            className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
+          >
+            <X className='h-4 w-4' />
           </Button>
         </PopoverClose>
-        <form action={onSubmit} className="space-y-4">
-          <div className="space-y-4">
-            <FromPicker id="image" errors={fieldErrors} />
-            <FormInput id="title" label="Board Title" type="text" errors={fieldErrors} />
+        <form action={onSubmit} className='space-y-4'>
+          <div className='space-y-4'>
+            <FromPicker id='image' errors={fieldErrors} />
+            <FormInput id='title' label='Board Title' type='text' errors={fieldErrors} />
           </div>
-          <FormSubmit className="w-full">Create</FormSubmit>
+          <FormSubmit className='w-full'>Create</FormSubmit>
         </form>
       </PopoverContent>
     </Popover>
