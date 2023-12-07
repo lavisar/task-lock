ab8047304928a2e7853ed52dab175f550f9ac8f7
lavisar <sarlavi6969@gmail.com>
2023-12-07 22:58:47 +0700

    fix some bug

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -11,6 +11,7 @@ import { InputType, ReturnType } from './type'
 import { CreateAuditLog } from '@/lib/create-audit-log'
 import { ACTION, ENTITY_TYPE } from '@prisma/client'
 import { decreaseAvailableCount } from '@/lib/org-limit'
+import { checkSubcription } from '@/lib/subcription'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -20,6 +21,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       error: 'Unauthorized'
     }
   }
+  const isPro = await checkSubcription()
   const { id } = data
   let board
   try {
@@ -30,7 +32,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       }
     })
 
-    await decreaseAvailableCount()
+    if (!isPro) {
+      await decreaseAvailableCount()
+    }
 
     await CreateAuditLog({
       entityTitle: board.title,
